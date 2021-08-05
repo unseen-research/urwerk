d42f26a3615e371eb1fb31ad0c963a00884fc0b1
Jay Pork <jpork@mailbox.org>
2021-08-05 15:07:03 +0200

    refactor

--- src/main/scala/urwerk/command/Parameters.scala
+++ src/main/scala/urwerk/command/Parameters.scala
@@ -1,11 +1,35 @@
 package urwerk.command
 
 object Parameter: 
+
   trait Converter[A]:
-    def convert(value: String): A
+    def requireValue: Boolean
+    def apply(value: String): A
 
   given Converter[String] with {
-    def convert(value: String): String = value
+    val requireValue = true
+    def apply(value: String): String = 
+      if value.startsWith("-") then
+        throw new IllegalArgumentException()
+      else
+        value
+  }
+
+  given Converter[Int] with {
+    val requireValue = true
+    def apply(value: String): Int = value.toInt
+  }
+
+  given Converter[Unit] with {
+    val requireValue = false
+    def apply(value: String): Unit = ()
+  }
+
+  case class RawArg(value: String)
+
+  given Converter[RawArg] with {
+    val requireValue = true
+    def apply(value: String): RawArg = RawArg(value)
   }
 
 import Parameter.Converter  
@@ -14,30 +38,32 @@ class Parameter[A, B](val names: Seq[String],
     val arity: (Int, Int), 
     val default: Option[A], 
     val config: B,
-    val collectOp: PartialFunction[(A, B), B],
-    val converter: Parameter.Converter[A]):
+    val converter: Parameter.Converter[A],
+    val collectOp: (A, B) => B):
   
   def default(value: A): Parameter[A, B] = copy(default = Some(value))
 
   def collect(op: (A, B) => B): Parameter[A, B] = 
-    copy(collectOp = {
-      case (value, config) => op(value, config)})
 
-  def doCollect(value: String): B = 
-    val _val = converter.convert(value)
+    val pf: (A, B) => B = 
+      case (value: A, config) => op(value, config)
+    copy(collectOp = pf)
+
+  private[command] def doCollect(value: String): B = 
+    val _val = converter(value)
     collectOp(_val, config)
 
   private def copy(names: Seq[String] = names, 
       arity: (Int, Int) = arity, 
       default: Option[A] = default, 
       config: B = config,
-      collectOp: PartialFunction[(A, B), B] = collectOp, 
-      converter: Converter[_ >: A] = converter) = 
-    new Parameter(names, arity, default, config, collectOp, converter.asInstanceOf[Converter[A]])
+      converter: Converter[A] = converter,
+      collectOp: (A, B) => B = collectOp) = 
+    new Parameter(names, arity, default, config, converter, collectOp)
   
 object Parameters:
   def apply[A](config: A): Parameters[A] = new Parameters[A](config)
 
 class Parameters[A](config: A):
-  
-  def param[B](using converter: Parameter.Converter[B]): Parameter[B, A] = new Parameter(Seq(), (0, 1), None, config, PartialFunction.empty, converter)
+
+  def param[B](using converter: Parameter.Converter[B]): Parameter[B, A] = new Parameter(Seq(), (0, 1), None, config, converter, {(_, config) => config})
--- src/test/scala/urwerk/command/ParametersTest.scala
+++ src/test/scala/urwerk/command/ParametersTest.scala
@@ -1,17 +1,89 @@
 package urwerk.command
 
 import urwerk.test.TestBase
+import Parameter.RawArg
 
 class ParametersTest extends TestBase:
-  "create paramerters" in {
-    case class Config(value: Int)
+  case class Config[A](value: A)
 
+  "int parameter" - {
     val params = Parameters(Config(0))
     import params.*
 
-    val value = param[String]
-      .collect((value, config) => config.copy(value = value.toInt))
-      .doCollect("77")
+    "requires value" in {
+      param[Int].converter.requireValue should be (true)
+    }
 
-    value should be (Config(77))  
+    "collect value" in {
+      val value = param[Int]
+        .collect{case (value, config) => config.copy(value = value)}
+        .doCollect("-77")
+
+      value should be (Config(-77))  
+    }  
+
+    "illegal value" in {
+      intercept[IllegalArgumentException]{
+        param[Int]
+          .doCollect("abc")
+      }
+    }
+  }
+
+  "string parameter" - {
+    val params = Parameters(Config(""))
+    import params.*
+
+    "requires value" in {
+      param[String].converter.requireValue should be (true)
+    }
+
+    "collect value" in {
+      val value = param[String]
+        .collect{case (value, config) => config.copy(value = value)}
+        .doCollect("string value")
+
+      value should be (Config("string value"))  
+    }  
+
+    "illegal value" in {
+      intercept[IllegalArgumentException]{
+        param[String]
+          .doCollect("-illegal arg value")
+      }
+    }
+  }
+
+  "unit parameter" - {
+    val params = Parameters(Config(""))
+    import params.*
+
+    "not require value" in {
+      param[Unit].converter.requireValue should be (false)
+    }
+
+    "collect value" in {
+      val value = param[Unit]
+        .collect{case (value, config) => config.copy(value = s"Value=$value")}
+        .doCollect("77")
+
+      value should be (Config("Value=()"))  
+    }
+  }
+
+  "raw arg parameter" - {
+    val params = Parameters(Config(""))
+    import params.*
+
+    "not require value" in {
+      param[RawArg].converter.requireValue should be (true)
+    }
+
+    "collect value" in {
+      val value = param[RawArg]
+        .collect{case (RawArg(value), config) => config.copy(value = value)}
+        .doCollect("--param-name")
+
+      value should be (Config("--param-name"))  
+    }
   }

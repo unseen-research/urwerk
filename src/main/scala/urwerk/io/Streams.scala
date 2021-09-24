49dc76a008228ffabb342e17d370b39023669968
Jay Pork <jpork@mailbox.org>
2021-09-24 14:19:57 +0200

    refactor

---
+++ src/main/scala/urwerk/io/Streams.scala
@@ -0,0 +1,48 @@
+package urwerk.io
+
+import java.io.InputStream
+import java.nio.channels.ReadableByteChannel
+import java.nio.channels.Channels
+
+import scala.annotation.tailrec
+
+import urwerk.source.Source
+import urwerk.source.Sink
+import java.nio.ByteBuffer
+
+object Stream:
+  val DefaultBufferSize: Int = 4096 * 2
+
+trait StreamOps:
+  extension (inputStream: InputStream)
+    def toSource: Source[ByteString] =
+      toSource(Stream.DefaultBufferSize)
+
+    def toSource(blockSize: Int): Source[ByteString] = readBytes(inputStream, blockSize)
+
+given StreamOps = new StreamOps{}
+
+private def readBytes(inputStream: InputStream, blockSize: Int): Source[ByteString] =
+  Source.using(Channels.newChannel(inputStream), _.close){channel =>
+    Source.create[ByteString]{sink =>
+      val buffer: ByteBuffer = ByteBuffer.allocate(blockSize)
+      readBytes(channel, buffer, sink, blockSize)
+    }
+  }
+
+@tailrec
+private def readBytes(
+    channel: ReadableByteChannel,
+    buffer: ByteBuffer,
+    sink: Sink[ByteString],
+    blockSize: Int): Unit =
+  val size = channel.read(buffer)
+
+  if size < 0 then
+    sink.complete()
+  else
+    buffer.flip()
+    if (buffer.limit() > 0) {
+      sink.next(ByteString.from(buffer))
+    }
+    readBytes(channel, buffer.clear(), sink, blockSize)
---
+++ src/test/scala/urwerk/io/StreamsTest.scala
@@ -0,0 +1,32 @@
+package urwerk.io
+
+import scala.util.Random
+
+import urwerk.test.TestBase
+import java.io.ByteArrayInputStream
+
+class StreamsTest extends TestBase:
+  "input stream to source" in {
+    val blockSize = Stream.DefaultBufferSize
+    val givenBytes = Random.nextBytes(blockSize * 3)
+
+    val givenByteStrings = Seq(
+      ByteString.unsafeWrap(givenBytes, 0, blockSize),
+      ByteString.unsafeWrap(givenBytes, blockSize, blockSize),
+      ByteString.unsafeWrap(givenBytes, blockSize * 2, blockSize))
+
+    val actualByteStrings = ByteArrayInputStream(givenBytes)
+      .toSource.toSeq.block
+
+    actualByteStrings should be (givenByteStrings)
+  }
+
+  "input stream to source with custom block size" in {
+    val givenBytes = Random.nextBytes(100)
+    val givenByteStrings = givenBytes.map(ByteString(_))
+
+    val actualByteStrings = ByteArrayInputStream(givenBytes)
+      .toSource(1).toSeq.block
+
+    actualByteStrings should be (givenByteStrings)
+  }

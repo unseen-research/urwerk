02636cec142324d0ce521240e7fa2f623ad16962
Jay Pork <jpork@mailbox.org>
2021-10-22 16:49:24 +0200

    refactor

--- src/main/scala/urwerk/app/App.scala
+++ src/main/scala/urwerk/app/App.scala
@@ -1,17 +1,32 @@
 package urwerk.app
 
-import scala.util.control.NonFatal
+import scala.collection.immutable.ArraySeq
 
 import urwerk.source.Optional
 import urwerk.source.Source
 import urwerk.io.ByteString
 import urwerk.io.Streams.*
+import urwerk.app.App.ExitStatus
 
-type Main = Source[ByteString] => Source[Either[ByteString, ByteString]]
 
-trait App(mainFn: Main):
+type Main = Seq[String] => Source[Either[ByteString, ByteString]]
+
+object App:
+  object ExitStatus:
+    type WithExitStatus = {
+      def exitStatus: Int
+    }
+
+    def unapply(error: Throwable): Option[Int] = error match
+      case es: ExitStatus =>
+        Some(es.exitStatus)
+      case _ => None
+
+  trait ExitStatus(val exitStatus: Int)
+
+class App(mainFn: Main):
   def main(args: Array[String]): Unit =
-    val src = mainFn(Source())
+    val src = mainFn(ArraySeq.unsafeWrapArray(args))
       .doOnNext{
         case Left(out) =>
           System.out.write(out)
@@ -23,6 +38,9 @@ trait App(mainFn: Main):
     try
       src.block
     catch
+      case error@ExitStatus(status) =>
+        sys.exit(status)
       case error: Throwable =>
-        println("ERROR")
+        sys.exit(1)
+
 
--- src/test/scala/urwerk/app/AppTest.scala
+++ src/test/scala/urwerk/app/AppTest.scala
@@ -14,16 +14,27 @@ import urwerk.app.command.Command
 import urwerk.app.command.Parameters
 import urwerk.io
 import scala.util.Random
+import urwerk.app.App.ExitStatus
+import org.slf4j.LoggerFactory
 
-val out = Source[ByteString]()
-val err = Source[ByteString]()
 
+
+//.setProperty(LoggerFactory.STATIC_LOGGER_BINDER_CLASS_PROPERTY, "org.slf4j.MyBinding");
 class AppTest extends TestBase:
+  sys.props.put("reactor.logging.fallback", "jdk")
+
   given ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool)
 
+  "exit status unapply" in {
+    new IllegalArgumentException("exit status message") with ExitStatus(77) match
+      case error@ExitStatus(status) =>
+        error.getMessage should be ("exit status message")
+        status should be(77)
+  }
+
   "run" in {
-    val output = javaExec.param("any-param", "XX***********XX").jointOutput.mkString.block
-    output should be (s"Greetings${nl}Mortal${nl}")
+    val output = javaExec.args("arg1", "arg2").jointOutput.mkString.block
+    output should be (s"Out: arg1Err: arg1Out: arg2Err: arg2")
   }
 
 val nl = System.lineSeparator
@@ -37,8 +48,8 @@ val javaExec = Exec(execPath)
 val params = Parameters[Seq[String]]
 import params.*
 
-def mainSrc(in: Source[ByteString]) =
-  Source(Right("abc"), Left("ABC"), Right("def"), Left("DEF"))
+def mainSrc(args: Seq[String]) =
+  Source.from(args.flatMap(arg => Seq(Right(s"Out: $arg"), Left(s"Err: $arg"))))
     .map(_.map(ByteString.from)
       .left
       .map(ByteString.from))

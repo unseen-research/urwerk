f7d15a6dac2ad4cea8224eb3758bd97befb9a9a2
Jay Pork <jpork@mailbox.org>
2021-10-25 15:16:02 +0200

    refactor

--- src/test/scala/urwerk/app/AppTest.scala
+++ src/test/scala/urwerk/app/AppTest.scala
@@ -1,20 +1,19 @@
 package urwerk.app
 
-import urwerk.test.TestBase
-import urwerk.source.Source
-import urwerk.io.ByteString
-import urwerk.io.file.Path
-import urwerk.source.Singleton
-import java.time.Instant
-import scala.concurrent.duration.Duration
-import urwerk.system.Exec
-import scala.concurrent.ExecutionContext
 import java.util.concurrent.Executors
+
+import scala.concurrent.ExecutionContext
+
+import urwerk.app.App.ExitStatus
 import urwerk.app.command.Command
 import urwerk.app.command.Parameters
-import urwerk.io
-import scala.util.Random
-import urwerk.app.App.ExitStatus
+import urwerk.io.ByteString
+import urwerk.io.file.Path
+import urwerk.source.Source
+import urwerk.test.TestBase
+
+import urwerk.system.Exec
+import urwerk.system.Process.Status.Terminated
 
 class AppTest extends TestBase:
 
@@ -28,27 +27,36 @@ class AppTest extends TestBase:
   }
 
   "run evaluate output" in {
-    val output = javaExec.args("arg1", "arg2").output.mkString.block
+    val output = testApp.args("arg1", "arg2").output.mkString.block
     output should be (s"Out: arg1,Out: arg2,")
   }
 
   "run evaluate error output" in {
-    val output = javaExec.args("arg1", "arg2").errorOutput.mkString.block
+    val output = testApp.args("arg1", "arg2").errorOutput.mkString.block
     output should be (s"Err: arg1,Err: arg2,")
   }
 
   "run evaluate joint output" in {
-    val output = javaExec.args("arg1", "arg2").jointOutput.mkString.block
+    val output = testApp.args("arg1", "arg2").jointOutput.mkString.block
     output should be (s"Out: arg1,Err: arg1,Out: arg2,Err: arg2,")
+  }
+
+  "run with non zero exit code" in {
+    val status = testAppError.args("77").status.last.block
+    status should be (Terminated(77))
   }
 
 val nl = System.lineSeparator
 
 val execPath = Path(sys.props("java.home") + "/bin/java").toAbsolutePath
 
-val javaExec = Exec(execPath)
+val testApp = Exec(execPath)
   .param("--class-path", sys.props("java.class.path"))
   .arg("urwerk.app.TestApp")
+
+val testAppError = Exec(execPath)
+  .param("--class-path", sys.props("java.class.path"))
+  .arg("urwerk.app.TestAppError")
 
 val params = Parameters[Seq[String]]
 import params.*
@@ -60,3 +68,8 @@ def mainSrc(args: Seq[String]) =
       .map(ByteString.from))
 
 object TestApp extends App(mainSrc)
+
+def mainSrcError(args: Seq[String]) =
+  Source.error(new IllegalArgumentException with ExitStatus(args(0).toInt))
+
+object TestAppError extends App(mainSrcError)

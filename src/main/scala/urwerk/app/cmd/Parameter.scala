099d895be9f7e0e729efa0a7c0c6a96cfa2967be
Jay Pork <jpork@mailbox.org>
2021-12-08 18:21:01 +0100

    refactor

--- src/main/scala/urwerk/app/cmd/Parameter.scala
+++ src/main/scala/urwerk/app/cmd/Parameter.scala
@@ -3,13 +3,19 @@ package urwerk.app.cmd
 object Parameter:
   type ConfigProvider[A] = () => A
 
-class Parameter[A](val default: Option[A], val applyOp: A => _):
+  def apply[A](): Parameter[A] = new Parameter(None, (x: A) => ???)
+
+class Parameter[A](val default: Option[A], private val applyOp: A => _):
   import Parameter.*
 
-  def default(value: A): Parameter[A] = Parameter[A](default = Some(value), applyOp)
+  def default(value: A): Parameter[A] = new Parameter[A](default = Some(value), applyOp)
 
-  def onApply[B](op: (B, A) => B)(using config: ConfigProvider[B]): Parameter[A] = ???
+  def onApply[B](op: (B, A) => B)(using config: ConfigProvider[B]): Parameter[A] =
+    val _applyOp = (value: A) => op(config(), value)
+    new Parameter(default = default, applyOp = _applyOp)
 
+  def apply[B](value: A): B = applyOp(value).asInstanceOf[B]
 
+end Parameter
 
 type Param[A] = Parameter[A]
--- src/test/scala/urwerk/app/cmd/ParameterTest.scala
+++ src/test/scala/urwerk/app/cmd/ParameterTest.scala
@@ -4,8 +4,17 @@ import urwerk.test.TestBase
 
 class ParameterTest extends TestBase:
 
+  import Parameter.*
+
   "default value" in {
-    val param = Parameter[String](None).default("some value")
+    val param = Parameter[String]().default("some value")
 
     param.default.get should be("some value")
+  }
+
+  "on apply" in {
+    case class Config(value: String)
+    given ConfigProvider[Config] = () => Config("")
+
+    Parameter[String]().onApply((config: Config, value) => config.copy(value = "test" + value)).apply("any value")
   }

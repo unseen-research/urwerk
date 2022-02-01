84948c42d68369c61768f47afd55143c5a452bc0
Jay Pork <jpork@mailbox.org>
2022-02-01 16:24:44 +0100

    refactor

--- src/main/scala/urwerk/cli/Command.scala
+++ src/main/scala/urwerk/cli/Command.scala
@@ -1,32 +1,26 @@
 package urwerk.cli
 
-import ParameterList.Position
-
-case class Command[C](config: C, parameterLists: Seq[ParameterList[C]], applyOp: C => Int | Any):
-  def apply(op: C => Int| Any): Command[C] = copy(applyOp = op)
-    
-  def execute(args: String*): Int = 
-
-    val (_config, pos) = parameterLists.foldLeft((config, Position(0, 0))){case ((config, pos), paramList) =>
-      paramList.collect(config, args)
-    }
-
-    applyOp(_config) match
-      case exitCode: Int => exitCode
-      case _ => 0
-
-  def parameterList(param: ConfigEvidence[C] ?=> Parameter[?, C], params: ConfigEvidence[C] ?=> Parameter[?, C]*): Command[C] = 
-    val paramList = ParameterList(param, params*)
-    copy(parameterLists = parameterLists :+ paramList)
-
 object Command: 
+  
+  sealed trait Setting
 
-  // trait Setting[V]: 
-  //   def value: V
+  class ParameterListFactory[C]():
+    def := (param: Seq[Parameter[?, C]]): ParameterListSetting[C] = ???
+  
+  case class ParameterListSetting[C](paramList: ParameterList[C]) extends Setting
 
-  trait Config[A]: 
-      type B = A
+  class DescriptionSettingFactory():
+    def := (description: String): DescriptionSetting = ???
 
-      def value: B
+  case class DescriptionSetting(description: String) extends Setting
 
-  def apply[C](config: C): Command[C] = new Command(config, Seq(), _ => 1)
+  def description: DescriptionSettingFactory = ???
+
+  def paramList[C](using ConfigEvidence[C])(name: String): ParameterListFactory[C] = 
+    ???
+
+  def apply[C](param: ConfigEvidence[C] ?=> Setting, params: ConfigEvidence[C] ?=> Setting*): Command[C] = 
+    ???
+
+class Command[C](val paramLists: Seq[ParameterList[C]]):
+  import Command.*
--- src/test/scala/urwerk/cli/CommandTest.scala
+++ src/test/scala/urwerk/cli/CommandTest.scala
@@ -1,35 +1,18 @@
 package urwerk.cli
 
-import urwerk.cli.Parameter.param
 import urwerk.test.TestBase
 
-case class Config(value: String = "")
+import Command.*
+import Parameter.param
 
 class CommandTest extends TestBase:
   
-  "execute with apply returning int" in {
-    val cmd = Command(Config("7")).apply{config => 
-      config.value.toInt
-    }
+  "command" in {
+    val cmd = Command[Seq[String]](
+      paramList("global") := Seq(
+        param[Int], param[Int], param[Int], param[Int]),
+      description := "some command description")
 
-    cmd.execute() should be (7)
   }
 
-  "execute with apply returning unit" in {
-    val cmd = Command(Config("7")).apply{config => 
-      "something"
-    }
-
-    cmd.execute() should be (0)
-  }
-
-  "execute with parameter" in {
-    val cmd = Command(Config("7")).parameterList(
-          param[String]("param", "p"){(value, config) => 
-            Config(value)
-        })
-      .apply(config => config.value.toInt)
-    
-    cmd.execute("--param", "8") should be (8)
-  }
-
+  
